5d943b1a546cecd4a2ab729768210f289b4332a6
holistictechnologies12 <69184705+holistictechnologies12@users.noreply.github.com>
2020-08-04 11:12:44 +0500

    index

---
+++ precache-manifest.e2ae538213bfa86786516e790de6d5a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4533b3410b3f0d2c1b80c4d01316c6ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd474214c1a4f4a0da43",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd474214c1a4f4a0da43",
+    "url": "/static/js/main.de8d077d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
